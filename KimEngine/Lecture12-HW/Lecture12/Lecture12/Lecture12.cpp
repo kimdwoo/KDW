de5f237f14e2874afa4e289af8571f899db2837c
Kimdongwoo <networld1227@gmail.com>
2024-05-27 22:26:11 +0900

    [KDW]12주차

---
+++ KimEngine/Lecture12-HW/Lecture12/Lecture12/Lecture12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <thread>  
+#include <chrono> 
+
+int main() {
+    int count = 1;
+
+    // while 문을 돌면서 1초 동안 대기
+    while (count <= 10) {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::cout << count << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+        std::cout << "지연 시간: " << elapsed.count() << "s\n";
+
+        ++count;
+    }
+
+    return 0;
+}
